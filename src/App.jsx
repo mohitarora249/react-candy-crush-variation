328e2e1fdb6c5b2ec3cd63a02f93f455c51898ec
mohitarora249 <mohitarora249@gmail.com>
2022-07-02 12:12:07 +0530

    added invalid move check and valid move color swap

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ function App() {
 
   const [squareBeingDragged, setSquareBeingDragged] = useState(null);
   const [squareBeingReplaced, setSquareBeingReplaced] = useState(null);
+  const [invalidMove, setInvalidMove] = useState("none");
 
   const getRowColFromEvent = (e) => [
     +e.target.getAttribute("data-row"),
@@ -28,26 +29,54 @@ function App() {
     setSquareBeingReplaced(getRowColFromEvent(e));
   };
 
-  const eventPreventDefault = (e) => {
+  const preventDefaultEvent = (e) => {
     e.preventDefault();
   };
 
   const dragEnd = () => {
-    if (isInvalidMove()) return;
+    if (isValidMove(squareBeingDragged, squareBeingReplaced)) {
+      const [srcSquareColor, destSquareColor] = [
+        game[squareBeingDragged[0]][squareBeingDragged[1]],
+        game[squareBeingReplaced[0]][squareBeingReplaced[1]],
+      ];
+      setGame((g) => {
+        g[squareBeingDragged[0]][squareBeingDragged[1]] = destSquareColor;
+        g[squareBeingReplaced[0]][squareBeingReplaced[1]] = srcSquareColor;
+        return [...g];
+      });
+    } else {
+      setInvalidMove("block");
+      setTimeout(() => {
+        setInvalidMove("none");
+      }, 1000);
+    }
     setSquareBeingDragged(null);
     setSquareBeingReplaced(null);
   };
 
-  const isInvalidMove = () => {
-    if (squareBeingDragged === null || squareBeingReplaced === null)
-      return true;
-    console.log("squareBeingDragged :: ", squareBeingDragged);
-    console.log("squareBeingReplaced :: ", squareBeingReplaced);
+  const getValidMoves = (srcSquare) => {
+    return [
+      [srcSquare[0] - 1, srcSquare[1]],
+      [srcSquare[0] + 1, srcSquare[1]],
+      [srcSquare[0], srcSquare[1] + 1],
+      [srcSquare[0], srcSquare[1] - 1],
+    ];
+  };
+
+  const isValidMove = (src, dest) => {
+    if (src === null || dest === null) return false;
+    const res = getValidMoves(src).find(
+      (v) => v[0] === dest[0] && v[1] === dest[1]
+    );
+    if (res) return true;
     return false;
   };
 
   return (
-    <div className="h-screen w-screen flex justify-center items-center">
+    <div className="h-screen w-screen flex flex-col justify-center items-center bg-gray-200">
+      <h1 className="font-bold">
+        Arrange 5 continous blocks of same color in 4 rows to win the game
+      </h1>
       <div>
         {game.map((row, idx) => (
           <div className="flex" key={idx}>
@@ -61,15 +90,21 @@ function App() {
                 data-color={r}
                 draggable
                 onDragStart={dragStart}
-                onDragOver={eventPreventDefault}
-                onDragEnter={eventPreventDefault}
-                onDragLeave={eventPreventDefault}
+                onDragOver={preventDefaultEvent}
+                onDragEnter={preventDefaultEvent}
+                onDragLeave={preventDefaultEvent}
                 onDrop={dragDrop}
                 onDragEnd={dragEnd}
               />
             ))}
           </div>
         ))}
+      </div>
+      <div
+        className="font-bold text-red-500 h-8 w-44 absolute bottom-2 shadow-2xl rounded-lg"
+        style={{ display: invalidMove }}
+      >
+        <div className="flex justify-center items-center">Invalid Move</div>
       </div>
     </div>
   );
